96f37f8a27cc3f4bc81793b712e10a0147931d15
Patrick <patrickdberry802@gmail.com>
2020-07-12 21:35:00 -0500

    Updating code to not produce warning about order of operations

--- src/p_spec.c
+++ src/p_spec.c
@@ -1184,7 +1184,7 @@ int EV_DoDonut(line_t*	line)
 	s2 = getNextSector(s1->lines[0],s1);
 	for (i = 0;i < s2->linecount;i++)
 	{
-	    if ((!s2->lines[i]->flags & ML_TWOSIDED) ||
+	    if (!(s2->lines[i]->flags & ML_TWOSIDED) ||
 		(s2->lines[i]->backsector == s1))
 		continue;
 	    s3 = s2->lines[i]->backsector;
